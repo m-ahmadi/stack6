eb7c933eb1d6c28578aa2d94a8ff7f384c6e0478
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-05-10 05:13:18 +0430

    used shorter syntax php and ROOT constant

--- public/_g.php
+++ public/_g.php
@@ -1,7 +1,9 @@
 <?php
+define('ROOT', 'stack6');
+
 function page_title($path) {
 	$arr = explode(DIRECTORY_SEPARATOR, $path);
-	echo ucfirst( end($arr) );
+	return ucfirst( end($arr) );
 }
 
 function layout($name) {
